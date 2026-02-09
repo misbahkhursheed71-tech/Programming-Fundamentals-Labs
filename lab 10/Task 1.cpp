393da3f5696e117ab91db4b374273addbe2f8151
misbahkhursheed71-tech <misbahkhursheed71@gmail.com>
2026-02-10 00:23:50 +0500

    Add files via upload

---
+++ lab 10/Task 1.cpp
@@ -0,0 +1,14 @@
+//Print all numbers from 1 to n
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the limit: ";
+	cin>>n;
+	for(int i=0;i<=n;i++)
+	{
+	cout<<i<<endl;
+	}
+	return 0;
+}
